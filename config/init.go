f8abbb387de49af97094452e06ff2b55b0829d55
Ming <1055274236@qq.com>
2023-05-31 21:56:17 +0800

    modify config

--- config/init.go
+++ config/init.go
@@ -2,6 +2,11 @@ package config
 
 import (
 	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -34,11 +39,50 @@ type ConfigStruct struct {
 
 func init() {
 	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./")
+	viper.AddConfigPath(getCurrentAbPath())
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("read config file failed, %v", err)
 	}
 	if err := viper.Unmarshal(&GlobalConfig); err != nil {
 		log.Printf("unmarshal config file failed, %v", err)
 	}
+}
+
+// 最终方案-全兼容
+func getCurrentAbPath() string {
+	dir := getCurrentAbPathByExecutable()
+	if strings.Contains(dir, getTmpDir()) {
+		return getCurrentAbPathByCaller()
+	}
+	return dir
+}
+
+// 获取系统临时目录，兼容go run
+func getTmpDir() string {
+	dir := os.Getenv("TEMP")
+	if dir == "" {
+		dir = os.Getenv("TMP")
+	}
+	res, _ := filepath.EvalSymlinks(dir)
+	return res
+}
+
+// 获取当前执行文件绝对路径
+func getCurrentAbPathByExecutable() string {
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, _ := filepath.EvalSymlinks(filepath.Dir(exePath))
+	return res
+}
+
+// 获取当前执行文件绝对路径（go run）
+func getCurrentAbPathByCaller() string {
+	var abPath string
+	_, filename, _, ok := runtime.Caller(0)
+	if ok {
+		abPath = path.Dir(filename)
+	}
+	return abPath
 }
