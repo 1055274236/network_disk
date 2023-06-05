1f767fc4b34e94ff5070442f9133b54fdfbd5188
Ming <1055274236@qq.com>
2023-06-05 21:09:11 +0800

    update filestoredao

--- dao/filestoredao/init.go
+++ dao/filestoredao/init.go
@@ -5,12 +5,13 @@ import "NetworkDisk/dao"
 // @params:
 // folder 文件夹名称
 // file 文件名称
+// fileType 文件类型
 // size 文件大小
 // md5 md5
 // sha1 sha1
 // createdUser 最初创建这个文件的用户
-func Add(folder string, file string, size int64, md5 string, sha1 string, createdUser int) (FileStoreTableStruct, error) {
-	item := FileStoreTableStruct{Folder: folder, File: file, Size: size, Md5: md5, Sha1: sha1, CreatedUser: createdUser}
+func Add(folder string, file string, fileType string, size int64, md5 string, sha1 string, createdUser int) (FileStoreTableStruct, error) {
+	item := FileStoreTableStruct{Folder: folder, File: file, Type: fileType, Size: size, Md5: md5, Sha1: sha1, CreatedUser: createdUser}
 	result := dao.MysqlDb.Create(&item)
 	return item, result.Error
 }
@@ -24,5 +25,11 @@ func GetByMd5AndSha1(md5 string, sha1 string) (FileStoreTableStruct, error) {
 func GetById(id int) (FileStoreTableStruct, error) {
 	temp := FileStoreTableStruct{Id: id}
 	result := dao.MysqlDb.First(&temp, id)
+	return temp, result.Error
+}
+
+func ChangeType(id int, fileType string) (FileStoreTableStruct, error) {
+	temp := FileStoreTableStruct{Id: id, Type: fileType}
+	result := dao.MysqlDb.Model(&temp).UpdateColumn("type", fileType)
 	return temp, result.Error
 }
--- dao/filestoredao/struct.go
+++ dao/filestoredao/struct.go
@@ -8,6 +8,7 @@ type FileStoreTableStruct struct {
 	Id          int       `gorm:"column:id;type:int(11);primary_key;AUTO_INCREMENT" json:"id"`
 	Folder      string    `gorm:"column:folder;type:varchar(255);NOT NULL" json:"folder"`
 	File        string    `gorm:"column:file;type:varchar(255);NOT NULL" json:"file"`
+	Type        string    `gorm:"column:type;type:varchar(15);NOT NULL" json:"type"`
 	Size        int64     `gorm:"column:size;type:bigint(20);NOT NULL" json:"size"`
 	Md5         string    `gorm:"column:md5;type:varchar(255);NOT NULL" json:"md5"`
 	Sha1        string    `gorm:"column:sha1;type:varchar(255);NOT NULL" json:"sha1"`
